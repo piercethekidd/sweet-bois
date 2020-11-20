2209691bffb18939dcf2c97524530eb6abcf2cf8
slobaddik <kiddevangelista@gmail.com>
2020-11-21 04:15:13 +0800

    add documentation for help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,12 +4,13 @@ module.exports = {
     description: 'List commands and their description',
     execute: (args, msg, commands) => {
         let message = ('```css\nList of commands:');
+        // List command descriptions from the description attribute
         _.forOwn(commands, (val, key) => {
             message += `\n_${key} - ${val.description}`
         });
         message += '```';
         msg.channel.send(message)
-        .then(message => console.log(`Replied to [${msg.author.tag}] with '${message}'`))
-        .catch(console.error);
+            .then(message => console.log(`Replied to [${msg.author.tag}] with '${message}'`))
+            .catch(console.error);
     },
 }
