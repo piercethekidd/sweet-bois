a09b0ac3929d4fc57d6be6b32052b377c22c896c
slobaddik <kiddevangelista@gmail.com>
2020-11-21 03:59:35 +0800

    edit feedme mechanics

--- src/commands/feedme.js
+++ src/commands/feedme.js
@@ -1,24 +1,32 @@
 const reddit = require('../util/reddit');
-const { SUBREDDIT } = process.env;
 
 module.exports = {
     description: 'Feed your thirsty ass bitch',
     execute: async (args, msg) => {
         try {
-            const res = await reddit.get(`/r/${SUBREDDIT}/hot`, {
-                count: 0,
-                limit: 1,
+            // throw error if args are missing
+            if (args.length === 0) {
+                const err_message = `Missing required arguments`;
+                await msg.channel.send(err_message);
+                throw err_message;
+            }
+
+            // search for results
+            const search_args = args.join('+');
+            const res = await reddit.get(`/r/${search_args}/hot`, {
+                limit: Math.floor(Math.random() * 100),
                 show: 'all',
             });
 
-            const { url, title } = res.data.children[res.data.children.length-1].data;
-            
-            const message = await msg.channel.send(`${title}\n${url}`);
+            // send to discord
+            const { data } = res.data.children[res.data.children.length-1];
+            const { url, title, id } = data;
+            const reddit_url = `(https://redd.it/${id})`;
+            const message = await msg.channel.send(`${title} ${reddit_url}\n${url}`);
             console.log(message.content);
 
         } catch (err) {
             console.error(err);
         }
-
     },
 }
