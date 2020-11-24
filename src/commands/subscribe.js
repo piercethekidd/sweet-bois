9251254fc36349075b5e16f16580dfef56e6d073
slobaddik <kiddevangelista@gmail.com>
2020-11-24 21:09:12 +0800

    add subscribe command

---
+++ src/commands/subscribe.js
@@ -0,0 +1,12 @@
+const { Scheduler } =  require('../util/scheduler');
+
+module.exports = {
+    description: 'Subscribes the current channel for some sweet boy content',
+    execute: (args, msg) => {
+        Scheduler.subscribe(msg.channel.id);
+        let message = 'Subscribed';
+        msg.channel.send(message)
+            .then(message => console.log(`Replied to [${msg.author.tag}] with '${message}'`))
+            .catch(console.error);
+    },
+}
