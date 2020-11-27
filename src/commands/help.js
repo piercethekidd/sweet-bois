d68cf5a335ac2be831298b7bef62187da3c8d143
slobaddik <kiddevangelista@gmail.com>
2020-11-27 21:30:21 +0800

    improve error handling

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,15 +2,18 @@ const _ = require('lodash');
 
 module.exports = {
     description: 'List commands and their description',
-    execute: (args, msg, commands) => {
+    execute: async (args, msg, commands) => {
         let message = ('```css\nList of commands:');
         // List command descriptions from the description attribute
         _.forOwn(commands, (val, key) => {
             message += `\n_${key} - ${val.description}`
         });
         message += '```';
-        msg.channel.send(message)
-            .then(message => console.log(`Replied to [${msg.author.tag}] with '${message}'`))
-            .catch(console.error);
+        try {
+            const serverMsg = await msg.channel.send(message);
+            console.log(`Replied to [${msg.author.tag}] with '${serverMsg}'`);
+        } catch (err) {
+            console.error(`Error: ${err.message}`);
+        }
     },
 }
