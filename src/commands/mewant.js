6e630528f68bbee577e59d19c7b05f25a4b31e75
slobaddik <kiddevangelista@gmail.com>
2020-11-27 19:30:42 +0800

    update mewant command to support general search option

--- src/commands/mewant.js
+++ src/commands/mewant.js
@@ -6,23 +6,47 @@ module.exports = {
     execute: async (args, msg) => {
         try {
             // throw error if args are missing
-            if (args.length === 0) {
+            if (args.length < 2) {
                 const err_message = `Missing required arguments`;
                 await msg.channel.send(err_message);
-                throw err_message;
+                return;
             }
 
-            // search for results
-            const q = args.join(' ');
-            const res = await reddit.get(`/search`, {
-                limit: 10,
-                show: 'all',
-                sort: 'top',
-                q
+            // check for --general flag
+            let isGeneralSearch = false;
+            args.forEach((arg, index) => {
+                if (arg === '--general' || arg === '--g') {
+                    isGeneralSearch = true;
+                    args.splice(index, 1);
+                }
             });
+
+            let res;
+            // enable general search
+            if (isGeneralSearch) {
+                res = await reddit.get(`/search`, {
+                    limit: 10,
+                    show: 'all',
+                    sort: 'top',
+                    q: args.join(' '),
+                });
+            }
+
+            // enable subreddit search
+            else {
+                const [subreddit] = args.splice(0, 1);
+                res = await reddit.get(`/r/${subreddit}/search/`, {
+                    limit: 10,
+                    show: 'all',
+                    sort: 'top',
+                    q: args.join(' '),
+                    restrict_sr: true,
+                });
+            }
 
             // extract necessary info from resulting array
             let posts = res.data.children;
+            if (posts.length === 0) throw 'Did not find any results';
             posts = _.map(posts, (post) => {
                 const { title, id, url, subreddit} = post.data;
                 return { id, title, url, subreddit };
